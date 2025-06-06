a9c5989836604f74c11805cf66b98259314b9f4c
rahul <nizarerahul7@gmail.com>
2025-06-06 17:58:12 +0530

    conecpts of formdata

---
+++ forms.js
@@ -0,0 +1,12 @@
+// !get forms
+
+// document.froms --> Array like collection
+// document.froms.my - get form by name
+
+// ! get form eleements
+
+// document.forms[0].elements.one ---> element with name one (collection in case of multiple with same name)
+// * shortHande = form.name
+
+//? Backreference:
+// elements of form poits back to the form --> element.form
